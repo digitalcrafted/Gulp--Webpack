b6d5372f95460cd7769ef9b7138dcecd7180650e
Francis <fran@prowork.me>
2017-05-21 10:26:18 +0100

    added foundation-js-components.js to list and require specific foundation components used in the project

---
+++ gulp-build/src/js/foundation-js-components.js
@@ -0,0 +1,36 @@
+/**
+ * Created by craft on 05/05/2017.
+ */
+var foundation_path = '../../node_modules/foundation-sites/js/';
+
+
+// Foundation core - needed if you want to use any of the components below 
+require(foundation_path + 'foundation.core');
+// Pick the components you need in your project 
+//require(foundation_path + 'foundation.util.box');
+require(foundation_path + 'foundation.util.keyboard');
+require(foundation_path + 'foundation.util.mediaQuery');
+require(foundation_path + 'foundation.util.motion');
+require(foundation_path + 'foundation.util.nest');
+require(foundation_path + 'foundation.util.timerAndImageLoader');
+require(foundation_path + 'foundation.util.touch');
+require(foundation_path + 'foundation.util.triggers');
+// require(foundation_path + 'foundation.abide');
+//require(foundation_path + 'foundation.accordion');
+//require(foundation_path + 'foundation.accordionMenu');
+//require(foundation_path + 'foundation.drilldown');
+require(foundation_path + 'foundation.dropdown');
+require(foundation_path + 'foundation.dropdownMenu');
+require(foundation_path + 'foundation.equalizer');
+//require(foundation_path + 'foundation.interchange');
+//require(foundation_path + 'foundation.magellan');
+require(foundation_path + 'foundation.offcanvas');
+require(foundation_path + 'foundation.orbit');
+//require(foundation_path + 'foundation.responsiveMenu');
+//require(foundation_path + 'foundation.responsiveToggle');
+//require(foundation_path + 'foundation.reveal');
+//require(foundation_path + 'foundation.slider');
+//require(foundation_path + 'foundation.sticky');
+//require(foundation_path + 'foundation.tabs');
+//require(foundation_path + 'foundation.toggler');
+//require(foundation_path + 'foundation.tooltip'); 
