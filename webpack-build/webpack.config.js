7d3872179507b1faf53246d9905320e929dfc719
Francis <fran@prowork.me>
2017-05-21 10:10:19 +0100

    minimal webpack configuration

---
+++ webpack-build/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+
+    },
+
+};
