f92bf303669d1d0e17add4fab99d9482f78855d7
Francis <fran@prowork.me>
2017-06-18 13:02:26 +0100

    hashed css and js outputs

--- webpack-build/webpack.config.js
+++ webpack-build/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+    filename: "../css/[name].[contenthash:8].css",
+});
 
 module.exports = {
     entry: {
         app: './src/js/app.js'
     },
     output: {
-        filename: '[name].js',
+        filename: '[name].[chunkhash:8].js',
         path: path.resolve(__dirname, 'dist/js')
 
     },
@@ -20,18 +24,27 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader", // compiles Sass to CSS
-                }]
+                use: extractSass.extract({
+                    use: [
+                        /* {
+                         loader: "style-loader" // creates style nodes from JS strings <= We wont need this after we use the extractsass plugin
+                         },*/
+                        {
+                            loader: "css-loader" // translates CSS into CommonJS
+                        },
+                        {
+                            loader: "sass-loader", // compiles Sass to CSS
+                        }
+                    ]
+                })
             }
 
             // Loaders for other file types can go here
 
         ],
     },
+    plugins: [
+        extractSass
+    ]
 
 };
