ad859b02b5054cbbd7fccf0c9d114663478307b1
Francis <fran@prowork.me>
2017-06-18 10:37:56 +0100

    added scss loaders to webpack config

--- webpack-build/webpack.config.js
+++ webpack-build/webpack.config.js
@@ -16,9 +16,19 @@ module.exports = {
                 exclude: [/node_modules/],
                 use: [{
                     loader: 'babel-loader',
-                    options: { presets: ['es2015'] },
+                    options: {presets: ['es2015']},
                 }],
             },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader", // compiles Sass to CSS
+                }]
+            }
 
             // Loaders for other file types can go here
 
